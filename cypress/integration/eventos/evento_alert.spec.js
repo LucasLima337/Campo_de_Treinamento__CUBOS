529e50eab6ed5df626519540251942e02765f0ab
Lucas Lima <luc.tech3337@gmail.com>
2021-05-15 22:39:53 -0300

    feat: cenário de teste para o botão alert foi criada

---
+++ cypress/integration/eventos/evento_alert.spec.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress"/>
+
+import loc from '../../support/locators'
+
+describe('LINK VOLTAR', () => {
+    before(() => {
+        cy.visit('../../../app/toTest.html')
+    })
+
+    it('deve exibir um alerta com uma mensagem específica', () => {
+        cy.mockAlert('alerta')
+
+        cy.get('#alert').click()
+
+        cy.get('@alerta')
+            .should('be.calledWith', 'Alert Simples')
+    })
+})
